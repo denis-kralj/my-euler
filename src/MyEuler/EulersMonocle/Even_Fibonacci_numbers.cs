7d209e5bcf74dd8ac5ede7138f9a7710969822c9
Denis Kralj <kralj.denis.zg@gmail.com>
2015-11-22 11:55:56 +0100

    added class for second problem, changed some return value type names (for some reason i like the full names rather then simplified ones)

---
+++ src/MyEuler/EulersMonocle/Even_Fibonacci_numbers.cs
@@ -0,0 +1,23 @@
+﻿using EulerMonocle;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EulersMonocle
+{
+    class Even_Fibonacci_numbers : IProblem
+    {
+        public const Int16 PROBLEM_NUMBER = 2;
+        public Int16 GetProblemNumber()
+        {
+            return PROBLEM_NUMBER;
+        }
+
+        public Solution GetSolution()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
