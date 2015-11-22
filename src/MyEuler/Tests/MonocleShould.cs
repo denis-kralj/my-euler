4aee86863b0139b21de8aded1b6e138020edca35
Denis Kralj <kralj.denis.zg@gmail.com>
2015-11-22 12:13:55 +0100

    Added console application so I can get the solutions on screen easy, removed test that has problem solution written explicitly (dont cheat people :D).

--- src/MyEuler/Tests/MonocleShould.cs
+++ src/MyEuler/Tests/MonocleShould.cs
@@ -9,14 +9,6 @@ namespace Tests
     public class MonocleShould
     {
         [TestMethod]
-        public void HaveAnAdequateSolutionToTheFirstProblem()
-        {
-            // silly test to see if everything works
-            var solution = Monocle.Instance[1].GetSolution();
-            Assert.AreEqual(solution.Result, "233168");
-        }
-
-        [TestMethod]
         public void HaveSameProblemNumberAsProblemIndex()
         {
             Int16 index = 1;
