f95280f9a98362eb6b67d55df8f3fde162fe5126
Denis Kralj <kralj.denis.zg@gmail.com>
2015-12-06 17:32:59 +0100

    added a solution for the 4th euler problem.

---
+++ src/MyEuler/EulersMonocle/Largest_palindrome_product.cs
@@ -0,0 +1,70 @@
+﻿using EulerMonocle;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EulersMonocle
+{
+    class Largest_palindrome_product : IProblem
+    {
+        public const Int16 PROBLEM_NUMBER = 4;
+
+        public short GetProblemNumber()
+        {
+            return PROBLEM_NUMBER;
+        }
+
+        public Solution GetSolution()
+        {
+
+            var result =
+                new Solution()
+                {
+                    ProblemNumber = PROBLEM_NUMBER,
+                    ProblemText = GetProblemText(),
+                    Result = GetResult()
+                };
+
+            return result;
+        }
+
+        private string GetResult()
+        {
+            var candidate = 0;
+            for (int i = 999; i > 99; i--)
+            {
+                for (int j = 999; j > 99; j--)
+                {
+                    if (isPalindrome(i * j) && i*j > candidate)
+                        candidate = (i * j);
+                }
+            }
+
+            return candidate.ToString();
+        }
+
+        private bool isPalindrome(int value)
+        {
+            Int32 i = 0, j = value.ToString().Length - 1;
+            
+            while(i < j)
+            {
+                if (value.ToString()[i++] != value.ToString()[j--]) return false;
+            }
+
+            return true;
+        }
+
+        private string GetProblemText()
+        {
+            return
+                "A palindromic number reads the same both ways. " +
+                "The largest palindrome made from the product of " +
+                "two 2 - digit numbers is 9009 = 91 × 99.  Find " +
+                "the largest palindrome made from the product of " +
+                "two 3 - digit numbers.";
+        }
+    }
+}
